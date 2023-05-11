ce4ea53d266e30241f4ffa36731035976fc1c877
Cesar Martins <71659015+cesarmartins1245@users.noreply.github.com>
2023-05-10 21:13:54 -0300

    barra de pesquisa

--- lib/pages/work_orders/work_orders_controller.dart
+++ lib/pages/work_orders/work_orders_controller.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class WorkOrdersController extends GetxController { 
-
+class WorkOrdersController extends GetxController {
+  var searchText = ''.obs;
+  var searchController = TextEditingController();
+  void updateTextValue(String value) {
+    searchText.value = value;
+  }
 }
--- lib/pages/work_orders/work_orders_page.dart
+++ lib/pages/work_orders/work_orders_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mobile_challenge/pages/work_orders/work_orders_controller.dart';
+import 'package:mobile_challenge/util/constants.dart';
 
 class WorkOrdersPage extends GetView<WorkOrdersController> {
   const WorkOrdersPage({Key? key}) : super(key: key);
@@ -9,15 +10,72 @@ class WorkOrdersPage extends GetView<WorkOrdersController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: AppConstants.secondaryColor,
         title: const Text('Work Orders'),
+        centerTitle: true,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios),
+          icon: const Icon(
+            Icons.arrow_back_ios_outlined,
+            size: 26,
+          ),
           onPressed: () {
             Get.back();
           },
         ),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(
+                      color: Colors.grey,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: controller.searchController,
+                            decoration: InputDecoration(
+                              hintText: 'Search work order',
+                              disabledBorder: InputBorder.none,
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            Get.snackbar('Search',
+                                'Searching for ${controller.searchController.text}');
+                          },
+                          icon: const Icon(
+                            Icons.search,
+                            size: 28,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
